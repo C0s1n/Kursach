b5e6dee5f6513aac20094487ff2e2400a2c82dba
C0s1n <78977410+C0s1n@users.noreply.github.com>
2022-05-16 10:31:23 +0700

    Update ViewModelBase.cs

--- asd/ViewModels/ViewModelBase.cs
+++ asd/ViewModels/ViewModelBase.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Kotin_Curs.ViewModels
+namespace C0s_Curs.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
